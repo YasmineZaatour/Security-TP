4159319a88aa6e18305c646af342a98131b02174
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-10-09 17:16:13 +0100

    Register function

--- Interface cryptage/main.py
+++ Interface cryptage/main.py
@@ -5,3 +5,16 @@ import sqlite3
 def db_connection ():
     conn = sqlite3.connect('users.db')
     return conn 
+
+#Register a new user 
+def register_user (username, password):
+    conn = db_connection()
+    c = conn.cursor()
+    try:
+        c.execute(''' INSERT INTO users (username, password) VALUES (?, ?) ''', (username, password))
+        conn.commit()
+        st.success("User registered successfully")
+    except sqlite3.IntegrityError:
+        st.error("Username already exists")
+    finally:
+        conn.close()
